f9c1621bd3e75ad3a698409af963297221130511
AugustDaniel <111312919+AugustDaniel@users.noreply.github.com>
2022-11-14 00:09:04 +0100

    Add function for single round and full game

--- script/script.js
+++ script/script.js
@@ -1,19 +1,57 @@
 function getComputerTest() {
     let x = Math.random() * 100;
     if (x <= 30) {
-        console.log("Rock")
+        return "Rock";
     }
     else if (x <= 60) {
-        console.log("Paper")
+        return "Paper";
     }
-    else console.log("Scissor")     
+    else {return "Scissor";  }   
 }
 
-function game(playerselection, computerselection = getComputerTest()) {
-    if (playerselection = "rock" && computerselection == "Scissor") {
-            console.log("You Win");
-    } else if (playerselection = "paper" && computerselection == "Rock") {
-            console.log("You Win");
-    }  else if (playerselection = "scissor" && computerselection == "Paper") {
-            console.log("You Win");
-    } else console.log("You Lose");
+function playRound(playerSelection, computerSelection) {
+    playerSelection = prompt("Choose your move:");
+    let playerSelect = playerSelection.toUpperCase();
+    computerSelection = getComputerTest().toUpperCase();
+    
+    console.log(computerSelection.toLowerCase());
+    
+    if (playerSelect == "ROCK"  && computerSelection== "SCISSOR") {
+            console.log("You Win " + playerSelect + " beats " + computerSelection);
+            return true
+    } 
+    else if (playerSelect == "PAPER" && computerSelection == "ROCK") {
+            console.log("You Win " + playerSelect + " beats " + computerSelection);
+            return true
+    }  
+    else if (playerSelect == "SCISSOR" && computerSelection == "PAPER") {
+            console.log("You Win " + playerSelect + " beats " + computerSelection);
+            return true
+    }   
+    else if (playerSelect == computerSelection) {
+            console.log("Tie");
+            return false
+    } 
+    else {console.log("You Lose " + computerSelection + " beats " + playerSelect); 
+            return false   }
+}
+
+function game() {
+    let playerScore = 0
+    let computerScore = 0
+    let i = 0
+    
+    while (i < 5) {
+        let result = playRound();
+        if (result === true) {playerScore++};
+        if (result === false) {computerScore++};
+        i++;
+    }
+    
+    if (playerScore > computerScore) {
+        console.log("You win the game")
+    }
+    else {
+        console.log("You lose the game")
+    }
+}
